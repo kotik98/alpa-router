edfc8b04d2d818435d92ba20f1b261095130f96a
Bogdan Zakharin <zaharin.98@mail.ru>
2023-01-24 13:13:15 +0300

    fix

--- runATR.js
+++ runATR.js
@@ -7,16 +7,16 @@ const { spawnSync } = require('child_process')
 
 const ATR = spawnSync('python3', ['ATRwithEMA.py']);
 
-// var fs = require('fs');
-// var util = require('util');
-// var logFile = fs.createWriteStream('log.txt', { flags: 'a' });
-//   // Or 'w' to truncate the file every time the process starts.
-// var logStdout = process.stdout;
-// console.log = function () {
-//   logFile.write(util.format.apply(null, arguments) + '\n');
-//   logStdout.write(util.format.apply(null, arguments) + '\n');
-// }
-// console.error = console.log;
+var fs = require('fs');
+var util = require('util');
+var logFile = fs.createWriteStream('log.txt', { flags: 'a' });
+  // Or 'w' to truncate the file every time the process starts.
+var logStdout = process.stdout;
+console.log = function () {
+  logFile.write(util.format.apply(null, arguments) + '\n');
+  logStdout.write(util.format.apply(null, arguments) + '\n');
+}
+console.error = console.log;
 
 var args = process.argv.slice(2);
 
